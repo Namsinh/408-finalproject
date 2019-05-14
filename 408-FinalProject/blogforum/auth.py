1d912f4a33a1ddcb0d66bdb9b554e2db99b0d2c8
Monique Namsinh <33501520+Namsinh@users.noreply.github.com>
2019-05-14 09:58:23 -0700

    Update auth.py

--- 408-FinalProject/blogforum/auth.py
+++ 408-FinalProject/blogforum/auth.py
@@ -1,9 +1,11 @@
 import functools
 
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
+
 
 from blogforum.db import get_db
 
